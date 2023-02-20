0fa13b19caeb01ebefa774e69dee663015016d6c
Andre Barbosa <andre.barbosa.076@ufrn.edu.br>
2023-02-20 01:37:14 -0300

    Changed the initial task state and added some new functions

--- src/components/TaskArea.tsx
+++ src/components/TaskArea.tsx
@@ -1,59 +1,79 @@
 import { Task } from "./Task";
 import styles from "./TaskArea.module.css";
-import { PlusCircle } from "phosphor-react";
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ListChecks, PlusCircle } from "phosphor-react";
+import { ChangeEvent, FormEvent, KeyboardEvent, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
-const taskList = [
-  {
-    id: uuidv4(),
-    content: "Terminar o projeto 01!",
-    isCompleted: true,
-  },
-  {
-    id: uuidv4(),
-    content: "Tudo pefeito",
-    isCompleted: false,
-  },
-  {
-    id: uuidv4(),
-    content: "Tudo tipado",
-    isCompleted: false,
-  },
-];
+export interface TaskProps {
+  id: string;
+  content: string;
+  isCompleted: boolean;
+}
 
 export function TaskArea() {
-  const [task, setTask] = useState(["Essa é sua primeira tarefa!"]);
-  const [newTask, setNewTask] = useState("");
+  const [task, setTask] = useState<TaskProps[]>([]);
+  const [newTaskContent, setNewTaskContent] = useState("");
 
-  const [countTasks, setCountTasks] = useState(1);
+  const [countTasks, setCountTasks] = useState(0);
   const [countDoneTasks, setCountDoneTasks] = useState(0);
+
+  const isNewTaskEmpty = newTaskContent.length === 0;
 
   function handleCreateNewTask(event: FormEvent) {
     event.preventDefault();
-
-    setTask([...task, newTask]);
-    setNewTask("");
+    setTask([
+      ...task,
+      {
+        id: uuidv4(),
+        content: newTaskContent,
+        isCompleted: false,
+      },
+    ]);
+    setNewTaskContent("");
   }
 
-  function handleNewTaskChange(event: ChangeEvent<HTMLTextAreaElement>) {
-    setNewTask(event.target.value);
-  }
-
-  function deleteTask(taskToDelete: string) {
-    const tasksWithoutDeletedOne = task.filter((task) => {
-      return task !== taskToDelete;
-    });
-
-    setTask(tasksWithoutDeletedOne);
-    setCountTasks((state) => {
-      return state - 1;
-    });
+  function handleNewTaskContent(event: ChangeEvent<HTMLTextAreaElement>) {
+    setNewTaskContent(event.target.value);
   }
 
   function handleCountTasks() {
     setCountTasks((state) => {
       return state + 1;
+    });
+  }
+
+  function handleToggleTaskCompletion(id: string) {
+    setTask((tasks) =>
+      tasks.map((task) => {
+        if (task.id === id) {
+          return {
+            ...task,
+            isCompleted: true,
+          };
+        }
+
+        return task;
+      })
+    );
+
+    setCountDoneTasks((state) => {
+      return state + 1;
+    });
+  }
+
+  function deleteTask(taskToDeleteId: string) {
+    const tasksWithoutDeletedOne = task.filter((task) => {
+      return task.id !== taskToDeleteId;
+    });
+
+    setTask(tasksWithoutDeletedOne);
+
+    setCountTasks((state) => {
+      return state - 1;
+    });
+
+    setCountDoneTasks((state) => {
+      return state - 1;
     });
   }
 
@@ -64,12 +84,17 @@ export function TaskArea() {
           <textarea
             name="task"
             placeholder="Add a new task"
-            value={newTask}
-            onChange={handleNewTaskChange}
+            value={newTaskContent}
+            onChange={handleNewTaskContent}
           />
 
           <footer>
-            <button title="add a task" onClick={handleCountTasks}>
+            <button
+              type="submit"
+              title="add a task"
+              onClick={handleCountTasks}
+              disabled={isNewTaskEmpty}
+            >
               Add
               <PlusCircle size={22} />
             </button>
@@ -95,9 +120,26 @@ export function TaskArea() {
       </div>
 
       <div className={styles.newTaskArea}>
-        {task.map((task) => {
-          return <Task key={task} content={task} onDeleteTask={deleteTask} />;
-        })}
+        {countTasks !== 0 ? (
+          task.map((task) => {
+            return (
+              <Task
+                key={task.id}
+                id={task.id}
+                content={task.content}
+                isCompleted={task.isCompleted}
+                onDeleteTask={deleteTask}
+                onCheck={handleToggleTaskCompletion}
+              />
+            );
+          })
+        ) : (
+          <div className={styles.emptyTasks}>
+            <ListChecks size={64} />
+            <strong>You have no tasks registered</strong>
+            <p>Create tasks and organize your to-do items</p>
+          </div>
+        )}
       </div>
     </div>
   );
